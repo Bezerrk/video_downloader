56ba792488a3beb0bb0ab09d52e1e9a83cd9fbe9
Bezerra <bezerragod@gmail.com>
2025-06-24 19:50:19 -0400

    changed the colors for a better look

--- main.py
+++ main.py
@@ -71,23 +71,24 @@ def download_yt_vid():
 download_screen = customtkinter.CTk()
 download_screen.geometry("400x300")
 download_screen.title("yt downloader")
-#download_screen.configure(fg_color="#E30D02")
+download_screen.configure(fg_color="#E30D02")
 download_screen.minsize(400,300)
 download_screen.maxsize(400,300)
 
-instruction_label = customtkinter.CTkLabel(download_screen, text="PASTE THE URL BELLOW",width=200,height=30)
-instruction_label.place(x=100,y=67)
+fonte = customtkinter.CTkFont(size=20)
+instruction_label = customtkinter.CTkLabel(download_screen, text="YOUTUBE DOWNLOADER",width=200,height=30,font=fonte)
+instruction_label.place(x=85,y=67)
 
-url_entry = customtkinter.CTkEntry(download_screen,placeholder_text="Paste URL here",width=220,height=40)
+url_entry = customtkinter.CTkEntry(download_screen,placeholder_text="Paste URL here",width=220,height=40,fg_color="#ffffff",text_color="black",placeholder_text_color="#9b9b9b",border_color="#9b9b9b")
 url_entry.place(x=90,y=130)
 
-download_button1 = customtkinter.CTkButton(download_screen,text="MP4",command=download_yt_vid,width=100,height=40)
+download_button1 = customtkinter.CTkButton(download_screen,text="MP4",command=download_yt_vid,width=100,height=40,fg_color="#ffffff",hover_color="#9b9b9b",text_color="black")
 download_button1.place(x=90,y=179)
 
-download_button2 = customtkinter.CTkButton(download_screen,text=".m4a",command=download_yt_audio,width=100,height=40)
+download_button2 = customtkinter.CTkButton(download_screen,text=".m4a",command=download_yt_audio,width=100,height=40,fg_color="#ffffff",hover_color="#9b9b9b",text_color="black")
 download_button2.place(x=210,y=179)
 
-history_download_button = customtkinter.CTkButton(download_screen,text="History",width=55,height=25)
+history_download_button = customtkinter.CTkButton(download_screen,text="History",width=55,height=25,fg_color="#ffffff",hover_color="#9b9b9b",text_color="black")
 history_download_button.place(x=12,y=14)
 
 download_screen.mainloop()
